787d9884fb2dd3cc28bca9da48c4a0555b86eecd
Dirc <e.e.cornet@gmail.com>
2023-11-22 16:02:38 +0100

    run app as kingpin CLI

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"github.com/alecthomas/kingpin/v2"
+)
+
+var (
+	dir     = kingpin.Flag("dir", "Dir to list files").Default(".").String()
+	csvFile = kingpin.Flag("csv", "CSV file to create or update").Default("files.csv").String()
+)
+
 func main() {
-	filename := "file_list.csv"
-	directory := "/home/cornet/go/src/github.com/dirc/file2csv/example"
-
-	UpdateCSV(directory, filename)
-
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	UpdateCSV(*dir, *csvFile)
 }
